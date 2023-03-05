6e33dc9398a45b985e18459c94318b9aab1ba258
Maxim Sinyukov <verero1173@yandex.ru>
2023-03-05 21:43:11 +0300

    feat: add literal types

--- app.ts
+++ app.ts
@@ -1,17 +1,24 @@
-function combine(one: string | number | boolean, two: string | number | boolean) {
+function combine(
+    one: string | number | boolean,
+    two: string | number | boolean,
+    resultConversion: 'as-number' | 'as-text',
+) {
   let result;
 
-  if (typeof one === 'number' && typeof two === 'number') {
-    result = one + two;
+  // if (typeof one === 'number' && typeof two === 'number' ||  resultConversion === 'as-numbe') { error as-numbe
+    if (typeof one === 'number' && typeof two === 'number' ||  resultConversion === 'as-number') {
+    result = +one + +two;
   } else {
     result = one.toString() + two.toString();
   }
 
-  return result;
+   return result;
 }
 
-const combinesAges = combine(30, 24);
+const combinesAges = combine(30, 24, 'as-number');
 
-const combineNames = combine('Valery', 'Borov');
+const combinesStringAges = combine('30', '24', 'as-text');
 
-console.log(combinesAges, combineNames);
+const combineNames = combine('Valery', 'Borov', 'as-text');
+
+console.log(combinesAges, combinesStringAges, combineNames);
