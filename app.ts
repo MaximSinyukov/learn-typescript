fdf4e8373253d8955ba77798ca94304d16d98462
Maxim Sinyukov <verero1173@yandex.ru>
2023-03-05 19:54:45 +0300

    feat: add array type

--- app.ts
+++ app.ts
@@ -9,8 +9,19 @@
 const person = {
   name: 'Max',
   age: 24,
+  hobbies: ['Sports', 'Cooking'],
 };
+
+let favoriteActivities: string[];
+// favoriteActivities = 'Sports'; get error
+// favoriteActivities = ['Sports', 1]; error from number set any[]
+favoriteActivities = ['Sports', '1'];
 
 // console.log(person.notexistprop); // get error
 
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+  // console.log(hobby.map()); // error
+}
